fce6a5be14cdb7b0bdb752de6320e2f830831109
pdpc1234 <pdpc1234@naver.com>
2017-11-12 14:09:49 +0900

    first commit

---
+++ samplegitproject/samplegitproject/samplegitproject.cpp
@@ -0,0 +1,13 @@
+// samplegitproject.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+    return 0;
+}
+
